984d4145bd90d3ada27f7b2bf0a959b466bfea71
Vladislav Petrosyan <PetrosyanV@dynacare.ca>
2025-11-13 12:38:28 -0500

    minor

--- BasicSyntax/Program.cs
+++ BasicSyntax/Program.cs
@@ -1,10 +1,10 @@
 ﻿using BasicSyntax;
 
-//1. ThreadCreation.ThreadCreationExample();
-//2. DivideAndConquer.DivideAndConquerExample();
-//3. WebServerSimulation.WebServerSimulationExample();
-//4. ThreadsSync.ThreadsSyncExample();
-//5. ExclusiveLock.ExclusiveLockExample();
-//6. TicketsBooking.TicketsBookingExample();
-MonitorExample.Counter(); MonitorExample.TicketsBooking();//7. 
+//1.  ThreadCreation.ThreadCreationExample();
+//2.  DivideAndConquer.DivideAndConquerExample();
+//3.  WebServerSimulation.WebServerSimulationExample();
+//4.  ThreadsSync.ThreadsSyncExample();
+//5.  ExclusiveLock.ExclusiveLockExample();
+//6.  TicketsBooking.TicketsBookingExample();
+//7.  MonitorExample.Counter(); MonitorExample.TicketsBooking();
 //7a. MonitorTryEnterExample.MonitorTryEnterExampleMain();
