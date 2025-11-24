598f45bd8833d1f7ac2c9ee6c92f852ebb5c53c8
Vladislav Petrosyan <PetrosyanV@dynacare.ca>
2025-11-24 12:27:52 -0500

    comment

--- BasicSyntax/Program.cs
+++ BasicSyntax/Program.cs
@@ -10,6 +10,7 @@
 //7a.  MonitorTryEnterExample.MonitorTryEnterExampleMain();
 //8.   MutexExample.MutexDemo();
 /*10.  
+ *     // Semaphore is global within the process, but SemaphoreSlim is local to the process
        using SemaphoreSlim semaphore = new SemaphoreSlim(initialCount: 3, maxCount: 3);
        var sem = new SemaphoreExample(semaphore);
        sem.SemaphoreExampleExec(); 
