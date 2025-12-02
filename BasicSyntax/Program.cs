208f69f36ce730a03ad387323c0ba46c7bd567ca
Vladislav Petrosyan <PetrosyanV@dynacare.ca>
2025-12-02 18:56:05 -0500

    минор фихес

--- BasicSyntax/Program.cs
+++ BasicSyntax/Program.cs
@@ -1,38 +1,39 @@
 ﻿using BasicSyntax;
 
-//1.   ThreadCreation.ThreadCreationExample();
-//2.   DivideAndConquer.DivideAndConquerExample();
-//3.   WebServerSimulation.WebServerSimulationExample();
-//4.   ThreadsSync.ThreadsSyncExample();
-//5.   ExclusiveLock.ExclusiveLockExample();
-//6.   TicketsBooking.TicketsBookingExample();
-//7.   MonitorExample.Counter(); MonitorExample.TicketsBooking();
-//7a.  MonitorTryEnterExample.MonitorTryEnterExampleMain();
-//8.   MutexExample.MutexDemo();
+/*1.   ThreadCreation.ThreadCreationExample();*/
+/*2.   DivideAndConquer.DivideAndConquerExample();*/
+/*3.   WebServerSimulation.WebServerSimulationExample();*/
+/*4.   ThreadsSync.ThreadsSyncExample();*/
+/*5.   ExclusiveLock.ExclusiveLockExample();*/
+/*6.   TicketsBooking.TicketsBookingExample();*/
+/*7.   MonitorExample.Counter(); MonitorExample.TicketsBooking();*/
+/*7a.  MonitorTryEnterExample.MonitorTryEnterExampleMain();*/
+/*8.   MutexExample.MutexDemo();*/
+
 /*10.  
-      // Semaphore is global within the process, but SemaphoreSlim is local to the process
-      using SemaphoreSlim semaphore = new SemaphoreSlim(initialCount: 3, maxCount: 3);
-      var sem = new SemaphoreExample(semaphore);
-      sem.SemaphoreExampleExec(); 
+       // Semaphore is global within the process, but SemaphoreSlim is local to the process
+       using SemaphoreSlim semaphore = new SemaphoreSlim(initialCount: 3, maxCount: 3);
+       var sem = new SemaphoreExample(semaphore);
+       sem.SemaphoreExampleExec(); 
 */
 /*11.
-     // AutoResetEvent 
-     using AutoResetEvent autoResetEvent = new AutoResetEvent(false);
-     var arEvent = new AutoResetEventExample(autoResetEvent);
-     // arEvent.AutoResetEventSingleExec();
-     arEvent.AutoResetEventMultyExec();
+       // AutoResetEvent 
+       using AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+       var arEvent = new AutoResetEventExample(autoResetEvent);
+       // arEvent.AutoResetEventSingleExec();
+       arEvent.AutoResetEventMultyExec();
 */
 /*12.
-    // ManualResetEvent 
-    using ManualResetEventSlim manualResetEvent = new ManualResetEventSlim(false);
-    var mrEvent = new ManualResetEventExample(manualResetEvent);
-    mrEvent.ManualResetEventExec();
+       // ManualResetEvent 
+       using ManualResetEventSlim manualResetEvent = new ManualResetEventSlim(false);
+       var mrEvent = new ManualResetEventExample(manualResetEvent);
+       mrEvent.ManualResetEventExec();
  */
 /*13.
-    // TwoWaySignalingInProducer 
-    using ManualResetEventSlim consumeEvent = new ManualResetEventSlim(false);
-    using ManualResetEventSlim produceEvent = new ManualResetEventSlim(true);
-    var producerExample = new TwoWaySignalingInProducer(consumeEvent, produceEvent);
-    producerExample.TwoWaySignalingInProducerExec();
+       // TwoWaySignalingInProducer 
+       using ManualResetEventSlim consumeEvent = new ManualResetEventSlim(false);
+       using ManualResetEventSlim produceEvent = new ManualResetEventSlim(true);
+       var producerExample = new TwoWaySignalingInProducer(consumeEvent, produceEvent);
+       producerExample.TwoWaySignalingInProducerExec();
 */
-    DeadlockExample.Exec(); //14.  
+/*14.  DeadlockExample.Exec();*/
